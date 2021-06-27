dee3429fb46baf157e30092f3b359d0bd94c92f1
Léo Pradel <pradel.leo@gmail.com>
2021-06-27 15:33:20 +0200

    feat: create api application (#2)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # stxstats
+
+## Running local API
+
+1. `cd api`
+2. `npm install`
+3. `npx prisma generate`
+4. `npm run start`
---
+++ api/src/index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const block = await prisma.blocks.findFirst({});
+  // console.log(JSON.stringify(block), 0, 2);
+  console.log(block);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
