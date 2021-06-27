193da67ceb3100b535365851fd9355ae7ae0062f
pradel <pradel.leo@gmail.com>
2021-06-27 15:36:57 +0200

    feat: number of transactions per day

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,31 @@
 import { PrismaClient } from "@prisma/client";
+import { addDays, isBefore, format } from "date-fns";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const block = await prisma.blocks.findFirst({});
-  // console.log(JSON.stringify(block), 0, 2);
-  console.log(block);
+  // TODO start first day of stacks 2.0
+  const startDate = new Date(2021, 0, 1);
+  const endDate = new Date();
+  let iteratorDate = startDate;
+
+  // Loop day by day between both dates
+  while (isBefore(iteratorDate, endDate)) {
+    const dayAfter = addDays(iteratorDate, 1);
+
+    const transactionsCount = await prisma.txs.count({
+      where: {
+        burn_block_time: {
+          gte: iteratorDate.getTime() / 1000,
+          lt: dayAfter.getTime() / 1000,
+        },
+      },
+    });
+
+    const dateFormatted = format(iteratorDate, "yyyy-MM-dd");
+    console.log(`Loop at ${dateFormatted} - ${transactionsCount} txs`);
+    iteratorDate = addDays(iteratorDate, 1);
+  }
 }
 
 main()
