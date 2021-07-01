0c68c153cfe34bfc35a3bcb7ec774dc9728abe40
pradel <pradel.leo@gmail.com>
2021-07-01 14:56:51 +0200

    feat: star on github text

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -64,8 +64,35 @@ const Home = ({ statsData }: any) => {
                   alt="Sigle logo"
                 />
               </a>
+              <p className="support">
+                If you like it consider giving us a{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="hsl(50 100% 48.5%)"
+                  className="star"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>{" "}
+                on{" "}
+                <a
+                  href="https://github.com/sigle/stxstats"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </p>
             </div>
             <div>
+              <a
+                className="donation"
+                href="https://explorer.stacks.co/address/SP6FRH7STC0G94N3PQ9BAGWF3W61R5BCEEEZB11A?chain=mainnet"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Donate
+              </a>
               <a
                 className="social-icon"
                 href="https://twitter.com/sigleapp"
