310d59c8ff7ab030e5e657bde99d42805cf50a7b
Pradel Léo <pradel.leo@gmail.com>
2021-08-02 15:13:46 +0200

    chart style

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,7 @@ import { Container } from "../src/ui/Container";
 import { Box } from "../src/ui/Box";
 import { Heading } from "../src/ui/Heading";
 import { Link } from "../src/ui/Link";
+import { Text } from "../src/ui/Text";
 
 const Home = ({ statsData }: any) => {
   // TODO create real button for this
@@ -52,31 +53,56 @@ const Home = ({ statsData }: any) => {
           <div>
             <Menu />
           </div>
-          <div className="chart">
-            <div className="chart-separator">
-              <div id="number-of-txs">
-                <p className="chart-description">Transactions per day</p>
-                <div className="chart-container">
-                  <div className="chart-reset">
-                    <NbTxsPerDay statsData={statsData.nbTxsPerDay} />
-                  </div>
-                </div>
-              </div>
-            </div>
+          <Box css={{ py: "$4" }}>
+            <Box id="number-of-txs">
+              <Heading as={"h3"} size={"xl"} css={{ fontWeight: 400 }}>
+                Transactions per day
+              </Heading>
+              <Text size={"sm"} css={{ mt: "$1" }}>
+                The chart shows the total number of transactions on the Stacks
+                blockchain daily.
+              </Text>
+              <Box css={{ position: "relative", height: 500, mt: "$4" }}>
+                <Box
+                  css={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    left: 0,
+                    top: 0,
+                  }}
+                >
+                  <NbTxsPerDay statsData={statsData.nbTxsPerDay} />
+                </Box>
+              </Box>
+            </Box>
 
-            <div className="chart-separator">
-              <div id="unique-addresses">
-                <p className="chart-description">Unique addresses over time</p>
-                <div className="chart-container">
-                  <div className="chart-reset">
-                    <UniqueAddressGrowingPerDay
-                      statsData={statsData.uniqueAddressGrowingPerDay}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            <Box id="unique-addresses" css={{ mt: 48 }}>
+              <Heading as={"h3"} size={"xl"} css={{ fontWeight: 400 }}>
+                Unique addresses over time
+              </Heading>
+              <Text size={"sm"} css={{ mt: "$1" }}>
+                The chart shows the total distinct numbers of address on the
+                Stacks blockchain and the increase in the number of address
+                daily.
+              </Text>
+              <Box css={{ position: "relative", height: 500, mt: "$4" }}>
+                <Box
+                  css={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    left: 0,
+                    top: 0,
+                  }}
+                >
+                  <UniqueAddressGrowingPerDay
+                    statsData={statsData.uniqueAddressGrowingPerDay}
+                  />
+                </Box>
+              </Box>
+            </Box>
+          </Box>
         </Box>
       </Container>
 
---
+++ client/src/ui/Text.ts
@@ -0,0 +1,22 @@
+import { styled } from "../stitches.config";
+
+export const Text = styled("p", {
+  variants: {
+    size: {
+      xs: {
+        fontSize: "$1",
+        color: "$gray9",
+        letterSpacing: "0.2px",
+      },
+      sm: {
+        fontSize: "$2",
+        color: "$gray9",
+        letterSpacing: "0.2px",
+      },
+      md: {
+        fontSize: "$3",
+        letterSpacing: "0.2px",
+      },
+    },
+  },
+});
