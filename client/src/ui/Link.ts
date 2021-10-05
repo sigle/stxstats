90f1ae563c9b05d70fb2e51daff56b3e6525df3e
Pradel Léo <pradel.leo@gmail.com>
2021-10-05 17:41:57 +0200

    Update Link.ts

--- client/src/ui/Link.ts
+++ client/src/ui/Link.ts
@@ -1,7 +1,6 @@
 import { styled } from "../stitches.config";
 
 export const Link = styled("a", {
-  fontFamily: "$merri",
   color: "$gray12",
   textDecoration: "none",
   borderBottom: "2px solid $gray12",
