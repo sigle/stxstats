25dd00e99c5807668971f88cf9a5cdd01d499d61
Pradel Léo <pradel.leo@gmail.com>
2022-01-12 18:43:05 +0100

    Update dailyTransactions.ts

--- api/src/api/dailyTransactions.ts
+++ api/src/api/dailyTransactions.ts
@@ -2,10 +2,6 @@ import { FastifyReply } from "fastify";
 import { prisma } from "../prisma";
 import { Result } from "../types/FileData";
 
-/**
- * TODO - start date as an option
- * TODO - end date as an option
- */
 export async function dailyTransactions(_: unknown, reply: FastifyReply) {
   const result = await prisma.$queryRaw<Result[]>`
   select to_timestamp(burn_block_time)::date as "date", count(*) as "value" from txs 
