9e178588f76eea030fa4898904be6cf799a521d7
gregogun <gregogun97@gmail.com>
2022-02-11 11:38:05 +0000

    feat: add utility functions

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,6 +1,41 @@
+import { differenceInMinutes, fromUnixTime, toDate } from 'date-fns';
+
 /**
  * @description Convert micro to stacks.
  * @param amountInMicroStacks - the amount of microStacks to convert
  */
 export const microToStacks = (amountInMicroStacks: string | number) =>
   Number(amountInMicroStacks) / Math.pow(10, 6);
+
+export const numberWithCommas = (x: string | number): string => {
+  let parts = x.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
+};
+
+export const getLastBlockTime = (lastBlockTimestamp: number): number => {
+  const currentTimestamp = Date.now();
+
+  return differenceInMinutes(
+    toDate(currentTimestamp),
+    fromUnixTime(lastBlockTimestamp)
+  );
+};
+
+export const numFormatter = (num: number): string | undefined => {
+  if (num > 1000000 && num < 1000000000) {
+    return (num / 1000000).toFixed(0) + 'M';
+  }
+
+  if (num > 1000000000) {
+    return (num / 1000000000).toFixed(0) + 'B';
+  }
+};
+
+export const getNextCycle = (block: number) => {
+  const timeInDays = (block * 10) / 1440;
+  if (timeInDays > 1) {
+    return Math.floor(timeInDays);
+  }
+  return timeInDays.toFixed(2);
+};
