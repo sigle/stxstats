c5d5fe6f2c79e4f78f9833bad11db362e549cd24
gregogun <gregogun97@gmail.com>
2022-02-11 11:46:04 +0000

    feat: add layout for dashboard

---
+++ client/src/components/DashboardLayout.tsx
@@ -0,0 +1,217 @@
+import { HamburgerMenuIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Box } from '../ui/Box';
+import { Button } from '../ui/Button';
+import { Container } from '../ui/Container';
+import { Dialog, DialogContent, DialogTrigger } from '../ui/Dialog';
+import { Flex } from '../ui/Flex';
+import { IconButton } from '../ui/IconButton';
+
+interface NavItemProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const NavItem = ({ href, children }: NavItemProps) => {
+  const router = useRouter();
+  return (
+    <Link href={href} passHref>
+      <Box
+        css={{
+          py: '$3',
+          px: '$3',
+          br: '$2',
+          backgroundColor: router.pathname === href ? '$gray5' : 'transparent',
+
+          '&:hover': {
+            backgroundColor: '$gray4',
+          },
+
+          '&:active': {
+            backgroundColor: '$gray5',
+          },
+        }}
+        target={href.includes('https') ? '_blank' : undefined}
+        rel="noreferrer"
+        as="a"
+      >
+        {children}
+      </Box>
+    </Link>
+  );
+};
+
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+export const DashboardLayout = ({ children }: LayoutProps) => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <IconButton
+            css={{
+              alignItems: 'center',
+              justifyContent: 'space-between',
+
+              '@lg': {
+                display: 'none',
+              },
+            }}
+          >
+            <HamburgerMenuIcon />
+          </IconButton>
+        </DialogTrigger>
+        <DialogContent>
+          <Flex
+            css={{ flex: 1 }}
+            as={'nav'}
+            direction={'column'}
+            justify={'between'}
+            align={'start'}
+          >
+            <Box>
+              {mounted === true ? (
+                theme === 'dark' ? (
+                  <Image
+                    height={38}
+                    width={81}
+                    src={'/images/new_logo_stxstats_white.svg'}
+                    alt="Stx stats logo"
+                    priority
+                  />
+                ) : (
+                  <Image
+                    height={38}
+                    width={81}
+                    src={'/images/new_logo_stxstats_black.svg'}
+                    alt="Stx stats logo"
+                    priority
+                  />
+                )
+              ) : (
+                <Box css={{ height: 38 }} />
+              )}
+              <Flex css={{ mt: '$10' }} direction={'column'} gap={'2'}>
+                <NavItem href={'/dashboard'}>Overview</NavItem>
+                <NavItem href={'#'}>Price</NavItem>
+                <NavItem href={'#'}>Mempool</NavItem>
+                <NavItem href={'#'}>Smart contracts</NavItem>
+                <NavItem href={'#'}>Wallets</NavItem>
+                <NavItem href={'#'}>Pools</NavItem>
+              </Flex>
+            </Box>
+            <Flex gap={'2'} direction={'column'}>
+              <NavItem href="https://twitter.com/sigleapp">Twitter</NavItem>
+              <NavItem href="https://discord.gg/PsMxnkqunJ">Discord</NavItem>
+              <Button
+                css={{ justifyContent: 'start' }}
+                as="a"
+                href="https://www.sigle.io/"
+                target="_blank"
+                size={'lg'}
+              >
+                Visit Sigle.io
+              </Button>
+              <Button
+                css={{ justifyContent: 'start', display: 'flex', gap: '$2' }}
+                variant={'ghost'}
+                onClick={() =>
+                  theme === 'dark' ? setTheme('light') : setTheme('dark')
+                }
+              >
+                Mode
+                <SunIcon />
+              </Button>
+            </Flex>
+          </Flex>
+        </DialogContent>
+      </Dialog>
+
+      <Container
+        css={{
+          display: 'none',
+
+          '@lg': {
+            display: 'block',
+            inset: 'unset',
+            py: 0,
+            mx: 0,
+          },
+        }}
+      >
+        <Flex
+          css={{ height: '100%' }}
+          as={'nav'}
+          direction={'column'}
+          justify={'between'}
+        >
+          <Box>
+            {mounted === true ? (
+              theme === 'dark' ? (
+                <Image
+                  height={38}
+                  width={81}
+                  src={'/images/new_logo_stxstats_white.svg'}
+                  alt="Stx stats logo"
+                  priority
+                />
+              ) : (
+                <Image
+                  height={38}
+                  width={81}
+                  src={'/images/new_logo_stxstats_black.svg'}
+                  alt="Stx stats logo"
+                  priority
+                />
+              )
+            ) : (
+              <Box css={{ height: 38 }} />
+            )}
+            <Flex css={{ mt: '$10' }} direction={'column'} gap={'2'}>
+              <NavItem href={'/dashboard'}>Overview</NavItem>
+              <NavItem href={'#'}>Price</NavItem>
+              <NavItem href={'#'}>Mempool</NavItem>
+              <NavItem href={'#'}>Smart contracts</NavItem>
+              <NavItem href={'#'}>Wallets</NavItem>
+              <NavItem href={'#'}>Pools</NavItem>
+            </Flex>
+          </Box>
+          <Flex gap={'2'} direction={'column'}>
+            <NavItem href="https://twitter.com/sigleapp">Twitter</NavItem>
+            <NavItem href="https://discord.gg/PsMxnkqunJ">Discord</NavItem>
+            <Button
+              css={{ justifyContent: 'start' }}
+              as="a"
+              href="https://www.sigle.io/"
+              target="_blank"
+              size={'lg'}
+            >
+              Visit Sigle.io
+            </Button>
+            <Button
+              css={{ justifyContent: 'start', display: 'flex', gap: '$2' }}
+              variant={'ghost'}
+              onClick={() =>
+                theme === 'dark' ? setTheme('light') : setTheme('dark')
+              }
+            >
+              Mode
+              <SunIcon />
+            </Button>
+          </Flex>
+        </Flex>
+      </Container>
+    </>
+  );
+};
---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,80 @@
+import { format, fromUnixTime, getUnixTime } from 'date-fns';
+import { DashboardLayout } from '../components/DashboardLayout';
+import { Overview } from '../components/Overview';
+import { DashboardData, PriceResult } from '../types/FileData';
+import { Container } from '../ui/Container';
+
+interface DashboardProps {
+  statsData: DashboardData;
+}
+
+const Dashboard = ({ statsData }: DashboardProps) => {
+  return (
+    <Container
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '$5',
+        py: '$5',
+
+        '@lg': {
+          gap: '$20',
+          flexDirection: 'row',
+          py: '$10',
+          height: '100vh',
+        },
+
+        '@xl': {
+          gap: '128px',
+        },
+      }}
+    >
+      <DashboardLayout />
+      <Overview statsData={statsData} />
+    </Container>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch(process.env.DASHBOARD_API_URL!);
+  const stxStats = await res.json();
+
+  const priceRes = await fetch(process.env.STX_PRICE_API_URL!);
+  const priceData = await priceRes.json();
+
+  const today = new Date();
+  const fromDate = getUnixTime(
+    new Date(new Date().setDate(today.getDate() - 30))
+  );
+  const toDate = getUnixTime(Date.now());
+
+  const rangeRes = await fetch(
+    `${process.env.RANGE_API_URL}from=${fromDate}&to=${toDate}`
+  );
+  const rangeData = await rangeRes.json();
+  const priceEvolutionRes: PriceResult[] = rangeData.prices;
+
+  let stxPriceEvolution: {}[] = [];
+
+  priceEvolutionRes.forEach((element) => {
+    const formattedDate = format(fromUnixTime(element[0] / 1000), 'yyyy-MM-dd');
+
+    const target = { date: formattedDate, value: element[1].toFixed(1) };
+
+    const toObj = Object.assign({}, target);
+
+    stxPriceEvolution.push(toObj);
+  });
+
+  return {
+    props: {
+      statsData: {
+        stxStats,
+        priceData,
+        stxPriceEvolution,
+      },
+    },
+  };
+}
+
+export default Dashboard;
