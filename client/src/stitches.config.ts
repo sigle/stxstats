ef5e61a3a93eb34b6393fb8e264bb2b7e50a6bc1
gregogun <gregogun97@gmail.com>
2022-02-11 11:34:44 +0000

    refactor: update font sizes

--- client/src/stitches.config.ts
+++ client/src/stitches.config.ts
@@ -105,8 +105,8 @@ export const {
       10: '40px',
     },
     fontSizes: {
-      1: '12px',
-      2: '14px',
+      1: '13px',
+      2: '15px',
       3: '16px',
       4: '21px',
       5: '24px',
