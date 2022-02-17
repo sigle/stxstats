a6f8a2d8b6e85646c28d2d6bf504d843a8c33b23
Léo Pradel <pradel.leo@gmail.com>
2022-02-17 13:32:11 +0100

    add cache to other endpoints

--- api/README.md
+++ api/README.md
@@ -2,6 +2,8 @@
 
 This service is responsible of generating the data used by the client. It expose this data via the http protocol on port 4000.
 
+A public API is available at https://api.stxstats.co/, note that the data is updated every 2 minutes.
+
 ## Endpoints
 
 ### Daily transactions count
--- api/src/api/dailyTransactions.ts
+++ api/src/api/dailyTransactions.ts
@@ -1,16 +1,26 @@
-import { FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { prisma } from '../prisma';
+import { apiCache } from './cache';
 
 interface DataResponse {
   date: string;
   txCount: number;
 }
 
-export async function dailyTransactions(_: unknown, reply: FastifyReply) {
-  const result = await prisma.$queryRaw<DataResponse[]>`
+export async function dailyTransactions(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const apiRoute = request.routerPath;
+  if (apiCache.has(apiRoute)) {
+    return reply.send(apiCache.get(apiRoute));
+  }
+
+  const response = await prisma.$queryRaw<DataResponse[]>`
   select to_timestamp(burn_block_time)::date as "date", count(*) as "txCount" from txs 
     where to_timestamp(burn_block_time)::date between '2021-01-01' and current_date
       group by 1`;
 
-  reply.send(result);
+  apiCache.set(apiRoute, response);
+  reply.send(response);
 }
