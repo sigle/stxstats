acb90dbbd8de4399d51ca2a27e08042e4eae8ec3
Léo Pradel <pradel.leo@gmail.com>
2022-03-05 14:42:13 +0100

    feat(client): improve number decimals (#87)

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -25,13 +25,9 @@ export const numFormatter = (num: number | undefined): string | undefined => {
     return;
   }
 
-  if (num > 1000000 && num < 1000000000) {
-    return (num / 1000000).toFixed(0) + 'M';
-  }
-
-  if (num > 1000000000) {
-    return (num / 1000000000).toFixed(0) + 'B';
-  }
+  return new Intl.NumberFormat('en-US', { notation: 'compact' })
+    .format(num)
+    .toString();
 };
 
 /**
