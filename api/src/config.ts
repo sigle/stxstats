a902eadd284c892f0731054be528e59567f9909c
Léo Pradel <pradel.leo@gmail.com>
2022-03-05 14:19:16 +0100

    chore: api remove rebuild logic (#86)

--- api/src/config.ts
+++ api/src/config.ts
@@ -2,7 +2,6 @@ import { cleanEnv, str } from 'envalid';
 
 export const config = cleanEnv(process.env, {
   TWITTER_API_KEY: str(),
-  REBUILD_WEBHOOK_URL: str(),
   TWITTER_API_SECRET_KEY: str(),
   TWITTER_ACCESS_TOKEN: str(),
   TWITTER_ACCESS_TOKEN_SECRET: str(),
